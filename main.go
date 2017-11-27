b1facc640c318c6aecd305bb766499aaf1f4c49b
Scott Finlay <scott.p.finlay@gmail.com>
2017-11-27 14:23:26 +0100

    Adding a landing page where you can enter the url without having to manually encode it.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/ynori7/reverse-proxy/client"
 	"github.com/ynori7/reverse-proxy/rewriter"
@@ -14,8 +15,29 @@ type myClient struct {
 
 func (c myClient) GetWithProxy(w http.ResponseWriter, r *http.Request) {
 	u := r.URL.Query().Get("u")
+	if u == "" {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte(`
+<html>
+<head><title>Scott's Go Proxy</title></head>
+<body>
+<form action='/' method='GET' style='width:1000px;margin:100px auto;'>
+	<input type='text' name='u' size='100' placeholder='Enter a URL here...'/>
+	<input type='submit' value='Go!'/>
+</form>
+</body>
+</html>
+		`))
+		return
+	}
+
+	if strings.Index(u, "http") < 0 {
+		u = "http://" + u //try to make the url valid and hope the server will redirect if necessary
+	}
+
 	requestedUrl, err := url.ParseRequestURI(u)
-	if u == "" || err != nil {
+	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Header().Set("Content-Type", "Text")
 		w.Write([]byte("Missing the 'u' query parameter. Please supply a URL to proxy."))
